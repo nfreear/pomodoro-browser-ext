765dda9ee1877ee1b621710e7178853e04860991
Nick Freear <nfreear@yahoo.co.uk>
2024-08-18 21:56:17 +0100

    Re-factor options to `OptionsStorage` class (#5)

--- options/options.js
+++ options/options.js
@@ -1,64 +1,88 @@
 /*!
-  Options page.
+  OptionsStorage class.
 
   © NDF, 08-August-2024.
 */
 
-const BROWSER = globalThis.browser || globalThis.chrome;
-
-const _storage = BROWSER.storage.local;
-const blockListForm = document.querySelector('#blockListForm');
-const durationForm = document.querySelector('#durationForm');
-
-blockListForm.addEventListener('submit', async (ev) => {
-  ev.preventDefault();
-
-  const url = ev.target.elements.url.value;
-  const listElem = ev.target.elements.list;
-
-  listElem.value += url + '\n';
-
-  const { blockList } = await _storage.get('blockList');
-
-  console.debug('submit:', url, blockList, listElem, ev);
-
-  if (isArray(blockList)) {
-    blockList.push(url);
-
-    await _storage.set({ blockList }); // , () => console.debug('storage ~ set:', blockList));
-    // _storage.set(blockList).then(() => console.debug('storage ~ set OK:', blockList));
-  } else {
-    await _storage.set({ blockList: [url] });
+class OptionsStorage {
+  constructor () {
+    this._blockListForm = document.querySelector('#blockListForm');
+    this._durationForm = document.querySelector('#durationForm');
   }
-});
 
-blockListForm.addEventListener('reset', async (ev) => {
-  ev.preventDefault();
+  get _browser () { return globalThis.browser || globalThis.chrome; }
+  get _storage () { return this._browser.storage.local; }
 
-  const { blockList } = await _storage.get('blockList');
+  get _blockListElem () { return this._blockListForm.elements.list; }
+  get _durationElem () { return this._durationForm.elements.duration; }
 
-  console.debug('reset:', blockList, ev.target.elements, ev);
+  handleUserEvents () {
+    this._blockListForm.addEventListener('submit', (ev) => this._blockListSubmitHandler(ev));
+    this._blockListForm.addEventListener('reset', (ev) => this._blockListResetHandler(ev));
 
-  /* await _storage.set({ blockList: [] });
-  ev.target.elements.list.value = '';
-  */
-});
+    this._durationElem.addEventListener('change', (ev) => this._durationChangeHandler(ev));
+  }
 
-durationForm.elements.duration.addEventListener('change', async (ev) => {
-  await _storage.set({ duration: ev.target.value });
+  async fromStorage () {
+    const { blockList, duration } = await this._storage.get();
 
-  console.debug('duration change:', ev.target.value, ev);
-});
+    this._blockListElem.value = blockList.join('\n');
+    this._durationElem.value = duration;
 
-(async () => {
-  // const blockList = await _storage.get('blockList');
-  const { blockList, duration } = await _storage.get();
+    console.debug('OptionsStorage ~ duration, blockList:', duration, blockList);
+  }
 
-  console.debug('storage ~ duration, blockList:', duration, blockList);
-})();
+  async _blockListSubmitHandler (ev) {
+    ev.preventDefault();
 
-function isArray (somevar) {
-  return somevar.constructor.name === 'Array';
+    const url = ev.target.elements.url.value;
+    const listElem = ev.target.elements.list;
+
+    listElem.value += url + '\n';
+
+    const { blockList } = await this._storage.get('blockList');
+
+    console.debug('OS ~ submit:', url, blockList, listElem, ev);
+
+    if (this._isArray(blockList)) {
+      blockList.push(url);
+
+      await this._storage.set({ blockList }); // , () => console.debug('storage ~ set:', blockList));
+      // _storage.set(blockList).then(() => console.debug('storage ~ set OK:', blockList));
+    } else {
+      await this._storage.set({ blockList: [url] });
+    }
+
+    await this.fromStorage();
+  }
+
+  async _blockListResetHandler (ev) {
+    ev.preventDefault();
+
+    const { blockList } = await this._storage.get('blockList');
+
+    await this._storage.set({ blockList: [] });
+    await this.fromStorage();
+
+    // ev.target.elements.list.value = '';
+
+    console.debug('OS ~ reset:', blockList, ev.target.elements, ev);
+  }
+
+  async _durationChangeHandler (ev) {
+    await this._storage.set({ duration: ev.target.value });
+
+    console.debug('OS ~ duration change:', ev.target.value, ev);
+  }
+
+  _isArray (somevar) {
+    return somevar.constructor.name === 'Array';
+  }
 }
+
+const optionsStorage = new OptionsStorage();
+
+optionsStorage.handleUserEvents();
+optionsStorage.fromStorage();
 
 console.debug('>> options.js');
